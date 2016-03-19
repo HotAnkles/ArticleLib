16512d10315bfdf8ac1fc6dabd33759ca91620d4
Christopher Michael <mets.chris@gmail.com>
2016-03-19 00:04:01 -0700

    Part 3 of ArticleLib.

--- public/app.js
+++ public/app.js
@@ -1,19 +1,34 @@
 var jquery = require('jquery');
+var fs = require('fs');
 
 var input = jquery('#url');
 input.val('paste your URL here');
+
+var request = require ('request')
+function downloadFromInternet(url) {
+  function afterrequest (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      fs.writeFile ('Library', body, function (){
+        readFromDisk ('Library')
+      })
+      console.log(body) // Show the HTML for the Google homepage.
+    } else {
+      alert('request failed')
+    }
+  }
+  request(url, afterrequest)
+}
+function readFromDisk (filename){
+  function afterreadfile (error, contents){
+    var body = jquery('body');
+    body.append (contents)
+  }
+  fs.readFile (filename, 'utf8', afterreadfile)
+
+}
 
 var form = jquery('[id=search]');
 form.on('submit', function() {
   downloadFromInternet(input.val())
   return false
 })
-
-var request = require ('request')
-function downloadFromInternet(url) {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage.
-    }
-  })
-}
